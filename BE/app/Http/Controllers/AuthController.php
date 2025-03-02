47a124e170493f2a72a20590517681a592cb89f4
Duy Anh <duya4986@gmail.com>
2025-03-03 00:07:25 +0700

    first commit

---
+++ BE/app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Đăng ký thành công'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('name', $request->name)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Sai tài khoản hoặc mật khẩu'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken('token')->plainTextToken]);
+    }
+}
